2d800f9553de6c04214fb71d8996c259fefdf866
Juan Francisco Maldonado Leon <juan.maldonado.leon@gmail.com>
2021-12-05 01:53:55 -0300

    url prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  endpoint: "https://api-basketball-pro-2vlkia2wsa-ue.a.run.app/api/v1/",
+  endpoint: "https://api-basketball-pro-pyltuofzyq-uk.a.run.app/api/v1/",
   production: true
 };
